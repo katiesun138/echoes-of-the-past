316772b3080f90ad8ba32a58afa0c72abba33c5d
tanaya <tanaya21@students.cs.ubc.ca>
2023-03-11 18:22:41 -0800

    wrote design for mock start page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Summary from './Summary';
 import GoogleApi from './GoogleApi';
--- my-app/src/Start.js
+++ my-app/src/Start.js
@@ -3,7 +3,25 @@ import React from "react";
 function Start(){
     return(
         <div>
-            <h1>Welcome to the starting page</h1>
+            <div>
+                <h3 className="header">echoes of the past</h3>
+                <h2 className="subHeader">a new way to learn, explore, and connect with your city</h2>
+            </div>
+
+            <div>
+                <h2 id="blurb">Explore the historical roots of your local community with suggested music.<br/>
+                    Choose a location or get updates as you move. </h2>
+            </div>
+
+            <div>
+                <a href="./GoogleApi">
+                    <button>Input Location</button>
+                </a>
+                
+                <a href="./Summary">
+                    <button>Live Location</button>
+                </a>
+            </div>
         </div>
     )
 }

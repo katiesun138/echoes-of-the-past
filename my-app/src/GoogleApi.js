be68f9d0c9dde3ac5b36c132a67981da01a63082
tanaya <tanaya21@students.cs.ubc.ca>
2023-03-12 00:57:16 -0800

    added live latitude, longitude tracking

--- my-app/src/GoogleApi.js
+++ my-app/src/GoogleApi.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function GoogleApi(){
+function GoogleApi(){ 
+
+    function componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(function(position) {
+        console.log("Latitude is :", position.coords.latitude);
+        console.log("Longitude is :", position.coords.longitude);
+      });
+    }
+  }
+
+  useEffect(() => componentDidMount());
+
     return(
         <div>
             <h1>Welcome to the GoogleApi page</h1>
+            <div>
+                <h3>Tracking your location...</h3>
+            </div>
         </div>
     )
 }
